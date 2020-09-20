6a662eb77efb78b5cc1338b562ada00c059422a0
shubhamymodi <shubhamymodi@gmail.com>
2020-09-20 15:57:10 +0530

    Updates

---
+++ precache-manifest.a71e067f3037f4398525abbe9f291982.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "932e6994be87e2b0d7f7d553f0eee7f8",
+    "url": "/COVID-19-Tracker/index.html"
+  },
+  {
+    "revision": "4745b010208ecf6c5043",
+    "url": "/COVID-19-Tracker/static/css/main.9aeefb75.chunk.css"
+  },
+  {
+    "revision": "c15dc6a02c85e665ae60",
+    "url": "/COVID-19-Tracker/static/js/2.8416e039.chunk.js"
+  },
+  {
+    "revision": "1ef8d540b3072f991ff256dfc61c7bcd",
+    "url": "/COVID-19-Tracker/static/js/2.8416e039.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4745b010208ecf6c5043",
+    "url": "/COVID-19-Tracker/static/js/main.8482c94f.chunk.js"
+  },
+  {
+    "revision": "f53963680f6f969729c6",
+    "url": "/COVID-19-Tracker/static/js/runtime-main.5babfe95.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/COVID-19-Tracker/static/media/image.d7265326.png"
+  }
+]);
